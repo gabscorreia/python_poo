f63e31363f823bf3a1b50f64bbd26fe7ee7c5ec6
CEITELABINFO <gabscorreia>
2024-08-19 10:42:47 -0300

    Simulação com 2 carros, modificando o método acelerar

--- src/frota.py
+++ src/frota.py
@@ -4,24 +4,36 @@ class Carro:
     cor : str
     odometro : 0.0
     motor_on : False
+    tanque: float
+    consumo_medio: float
 
     def __init__(self, modelo : str, marca : str, cor : str,
-                       odometro : float, motor : bool):
+                       odometro : float, motor : bool, tanque: float, consumo_medio: float):
         self.modelo = modelo
         self.marca = marca
         self.cor = cor
         self.odometro = odometro
         self.motor_on = motor
+        self.tanque = tanque
+        self.consumo_medio = consumo_medio
 
     def ligar(self):
-        if not self.motor_on:
+        if not self.motor_on and self.tanque > 0:
             self.motor_on = True
         else:
-            raise Exception("Erro: Motor já ligado!")
+            raise Exception("Erro: Motor já ligado, ou sem combustivel")
 
     def acelerar(self, velocidade : float, tempo : float):
         if self.motor_on:
-            self.odometro += velocidade * tempo
+            km = velocidade * tempo
+            litros = km/ self.consumo_medio
+            if self.tanque > litros:
+                self.tanque -= litros
+                self.odometro += km
+            else:
+                self.odometro += self.tanque * self.consumo_medio
+                self.tanque = 0
+                self.motor_on = False
         else:
             raise Exception("Erro: Não é possível acelerar! Motor desligado!")
 
--- src/main.py
+++ src/main.py
@@ -26,21 +26,24 @@ if __name__ == "__main__":
     nm_modelo = input('Digite o modelo: ')
     nm_marca = input('Digite a marca: ')
     nm_cor = input('Digite a cor: ')
+    litros = float(input('digite o nivel do tanque'))
+    consumo_medio = float(input('digite o consumo medio'))
 
-
-    carro1 = Carro(nm_modelo, nm_marca, nm_cor, 0, False)
+    carro1 = Carro(nm_modelo, nm_marca, nm_cor, 0, False, litros, consumo_medio)
 
     print('Cadastre O carro2')
     nm_modelo = input('Digite o modelo: ')
     nm_marca = input('Digite a marca: ')
     nm_cor = input('Digite a cor: ')
+    litros = float(input('digite o nivel do tanque'))
+    consumo_medio = float(input('digite o consumo medio'))
 
-    carro2 = Carro(nm_modelo, nm_marca, nm_cor, 0, False)
+    carro2 = Carro(nm_modelo, nm_marca, nm_cor, 0, False, litros, consumo_medio)
 
     '''
     Controlando o carro até ele atingir 10000 Km
     '''
-    while carro1.odometro < 360 and carro2.odometro<300:
+    while carro1.odometro < 360 and carro2.odometro < 300 and (carro1.tanque > 0 or carro2.tanque > 0):
         try:
             op_carro = 0
             while op_carro not in (1,2):
